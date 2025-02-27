196693c253792670bd5aef35e34e352d0c16afc8
aKqir24 <Akqir@protonmail.com>
2025-02-28 00:19:35 +0800

    Committed a change to UI.py & UI_Styles.css

--- UI.py
+++ UI.py
@@ -2,21 +2,24 @@ from sys import argv, exit
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QFrame, QPushButton, QWidget, QHBoxLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize as BTSize, Qt
+from cssutils import parseString
 
 class AddUIConfig(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Add Your Arrangement")
+        self.setStyleSheet("background-color: #242424")
         self.setGeometry( 168, 164, 538, 655 )
 
     def directoryDialog():
+        # TODO: Make A Label With Settings For The Arrangements
         pass
 
 class UIConfig(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Just Arrange")
-        self.setGeometry( 168, 164, 870, 560 )
+        self.setGeometry( 168, 164, 875, 560 )
         self.setStyleSheet("background-color: #242424")
         self.frontButtons()
         self.AddDisplayFrame()
@@ -44,13 +47,9 @@ class UIConfig(QMainWindow):
         SettingsButton.clicked.connect(self.AddUI)
 
         def setButtonsStyle():
-            unified_button_style =  "padding: 9px 14px; border: 1px solid #434343;"
-            individual_button_style = [ "background-color: #008170; border:1px solid #008170",
-                                        "background-color: #434343;",
-                                        "background-color: #434343;" ]
-            AddButton.setStyleSheet(unified_button_style+individual_button_style[0])
-            StartButton.setStyleSheet(unified_button_style+individual_button_style[1])
-            SettingsButton.setStyleSheet(unified_button_style+individual_button_style[2])
+            AddButton.setStyleSheet(readStyle()[0])
+            StartButton.setStyleSheet(readStyle()[1])
+            SettingsButton.setStyleSheet(readStyle()[2])
 
         return setButtonsStyle()
 
@@ -68,6 +67,20 @@ class UIConfig(QMainWindow):
         pass
 
 if __name__ == '__main__':
+    def readStyle():
+        button_style = []
+        with open('UI_styles.css', 'r') as styleFile:
+            readStyle = parseString(styleFile.read())
+            for rule in readStyle:
+                if rule.type == rule.STYLE_RULE and rule.selectorText == "#individual_button_0":
+                    buttonStyle = rule.cssText.replace("#individual_button_0", "QPushButton")
+                if rule.type == rule.STYLE_RULE and rule.selectorText == "#individual_button_1":
+                    buttonStyle = rule.cssText.replace("#individual_button_1", "QPushButton")
+                if rule.type == rule.STYLE_RULE and rule.selectorText == "#individual_button_2":
+                    buttonStyle = rule.cssText.replace("#individual_button_2", "QPushButton")
+                button_style.append(buttonStyle.replace("\n", ""))
+        return button_style
+
     program = QApplication(argv)
     interface = UIConfig()
     interface.show()
