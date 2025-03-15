c5ee904b90ee9ae18e5d4812bf6cff6bb7235be4
aKqir24 <Akqir@protonmail.com>
2025-03-15 22:09:51 +0800

    Updated The UI

--- Add_UI.py
+++ Add_UI.py
@@ -1,13 +1,31 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import QSize as BTSize, Qt
+from Start_ import QWidget, QLabel, BTSize, Qt
 
 class AddUIConfig(QWidget):
-    def __init__(self):
+    def __init__(self, STYLES):
         super().__init__()
         self.setWindowTitle("Add Your Arrangement")
         self.setStyleSheet("background-color: #242424")
         self.setGeometry( 295, 155, 538, 645 )
+        self.directoryDialog(STYLES)
 
-    def directoryDialog():
+    def directoryDialog(self, STYLES):
         # TODO: Make A Label With Settings For The Arrangements
+        tooltip_style = STYLES['Labels_Txt'][1].replace("QLabel:hover", "QToolTip")
+        tooltip_txt = " In What Folder To Put Your Arranged Files!!"
+        dir_txt_label = QLabel("Folder Directory:", self)
+        dir_txt_label.setStyleSheet(STYLES['Labels_Txt'][0]+tooltip_style)
+        dir_txt_label.setToolTip(tooltip_txt)
+        dir_txt_label.move(25, 25)
+
+class AddFunctions:
+    def Extensions():
+        pass
+
+    def OutputFolder():
+        pass
+
+    def KeyWord():
+        pass
+
+    def indiacate():
         pass
--- UI.py
+++ UI.py
@@ -1,38 +1,42 @@
-from sys import argv, exit
-from Add_UI import AddUIConfig
-from Settings_UI import SettingsUIConfig
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QFrame, QPushButton, QWidget, QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize as BTSize, Qt
-from cssutils import parseString
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QFrame, QPushButton, QWidget, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout
 
 class UIConfig(QMainWindow):
-    def __init__(self):
+    def __init__(self, STYLE_OWNER):
         super().__init__()
+        self.STYLES = STYLE_OWNER
+        self.center_widget = QWidget()
+        self.setCentralWidget(self.center_widget)
+        self.widgets_wrapper = QVBoxLayout(self.center_widget)
         self.setWindowTitle("Just Arrange")
         self.setGeometry( 168, 164, 875, 565 )
         self.setStyleSheet("background-color: #242424")
-        self.frontButtons()
-        self.searchArrange()
+        searchArea = self.searchArrange(STYLE_OWNER)
+        arrangeFrame = self.addArrangeFrame()
+        self.frontWidgets(STYLE_OWNER, searchArea, arrangeFrame)
 
     #? Open TopLevel Windows
     def openAddUI(self):
-        self.add_ui = AddUIConfig()
+        from Add_UI import AddUIConfig
+        self.add_ui = AddUIConfig(self.STYLES)
         self.add_ui.show()
 
     def openSettingsUI(self):
         #TODO: Make A Setting For Icons In buttons With Test or not
-        self.settings_ui = SettingsUIConfig()
+        from Settings_UI import SettingsUIConfig
+        self.settings_ui = SettingsUIConfig(self.STYLES)
         self.settings_ui.show()
 
     #? Button Setups
-    def frontButtons(self):
+    def frontWidgets(self, STYLES, searchArea, arrangeFrame):
         AddButton = QPushButton("", self)
         StartButton = QPushButton("", self)
         SettingsButton = QPushButton("", self)
-        AddButton.setFixedSize(52,52)
-        StartButton.setFixedSize(52,52)
-        SettingsButton.setFixedSize(52,52)
+        AddButton.setMinimumSize(42,42)
+        StartButton.setMinimumSize(42,42)
+        SettingsButton.setMinimumSize(60,60)
+        searchButton, searchBox = searchArea
 
         def setButtonsConfig():
             #? Icons
@@ -44,59 +48,57 @@ class UIConfig(QMainWindow):
             SettingsButton.setIconSize(BTSize(30, 30))
 
             #? Stylesheet
-            AddButton.setStyleSheet(readStyle()[0]+readStyle()[3])
-            StartButton.setStyleSheet(readStyle()[1]+readStyle()[4])
-            SettingsButton.setStyleSheet(readStyle()[1]+readStyle()[4])
-
-            #? Position
-            AddButton.move( 35, 35 )
-            StartButton.move( 100, 35 )
-            SettingsButton.move( 788, 35 )
+            AddButton.setStyleSheet(STYLES['Add_BT'])
+            StartButton.setStyleSheet(STYLES['Default_BT'])
+            SettingsButton.setStyleSheet(STYLES['Default_BT'])
 
             #? Commands
             AddButton.clicked.connect(self.openAddUI)
             StartButton.clicked.connect(self.openSettingsUI)
             SettingsButton.clicked.connect(self.openSettingsUI)
 
+            def addToLayout():
+                widgets_container = QHBoxLayout(self.center_widget)
+                widgets_container.setSpacing(8)
+                widgets_container.addWidget(AddButton)
+                widgets_container.addWidget(StartButton)
+                widgets_container.addWidget(searchBox)
+                widgets_container.addWidget(searchButton)
+                widgets_container.addWidget(SettingsButton)
+                widgets_container.setContentsMargins( 0, 0, 0, 8 )
+                self.widgets_wrapper.addLayout(widgets_container)
+                self.widgets_wrapper.setContentsMargins( 25, 25, 25, 25 )
+                self.widgets_wrapper.addWidget(arrangeFrame)
+                self.setLayout(self.widgets_wrapper)
+
+            return addToLayout()
         return setButtonsConfig()
 
-    def searchArrange(self):
+    def searchArrange(self, STYLES):
         searchBox = QLineEdit("", self)
         searchBox.setPlaceholderText("Search...")
-        searchBox.setGeometry( 165, 35, 550, 52 )
-        searchBox.setStyleSheet(readStyle()[2]+readStyle()[5])
+        searchBox.setStyleSheet(STYLES['Search_Box'])
+        searchBox.setMinimumHeight( 60 )
 
         searchButton = QPushButton("", self)
         searchButton.setIcon(QIcon("UI_icons\\search.svg"))
-        searchButton.setStyleSheet(readStyle()[1]+readStyle()[4])
+        searchButton.setStyleSheet(STYLES['Search_BT'])
         searchButton.setIconSize(BTSize(35, 35))
-        searchButton.setFixedSize( 52, 52 )
+        searchButton.setMinimumSize( 60, 60 )
         searchButton.move( 725, 35 )
         searchButton.clicked.connect( lambda : print(searchBox.text()) )
+        return (searchButton, searchBox)
 
-if __name__ == '__main__':
-    def readStyle():
-        button_style = []
-        with open('UI_styles.css', 'r') as styleFile:
-            readStyle = parseString(styleFile.read())
-            for index, rule in enumerate (readStyle):
-                style_rules = { 'Css-Ids': [ "#add_button", "#universal_button", "#searchbox" ],
-                                'Qt-Ids': [ "QPushButton", "QPushButton", "QLineEdit" ] }
+    def addArrangeFrame(self):
+        arrange_container = QFrame()
+        arrange_container.setMinimumSize(10,10)
+        arrange_container.setStyleSheet("background-color: #2F2F2F")
 
-                css_index_length = int(len(style_rules['Css-Ids']))
-                if index < css_index_length:
-                    if rule.type == rule.STYLE_RULE and rule.selectorText == style_rules['Css-Ids'][index]:
-                        buttonStyle = rule.cssText.replace(style_rules['Css-Ids'][index], style_rules['Qt-Ids'][index])
-                        button_style.append(str(buttonStyle.replace("\n", "")))
+        return arrange_container
 
-                else:
-                    if rule.type == rule.STYLE_RULE and rule.selectorText == style_rules['Css-Ids'][index-css_index_length]+":hover":
-                        buttonStyle_hover = rule.cssText.replace(style_rules['Css-Ids'][index-css_index_length]+":hover", style_rules['Qt-Ids'][index-css_index_length]+":hover")
-                        button_style.append(str(buttonStyle_hover.replace("\n", "")))
-
-        return button_style
-
+def initiateMainUI(STYLE_OWNER):
+    from sys import argv, exit
     program = QApplication(argv)
-    interface = UIConfig()
+    interface = UIConfig(STYLE_OWNER)
     interface.show()
     exit(program.exec_())
