196e6642ce1071ad183fc1256eceeff52e09d12c
aKqir24 <Akqir@protonmail.com>
2025-07-23 09:36:31 +0800

    move.py update

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-
-'''
 from shutil import move
 from os import path, makedirs, listdir
 
+keywords=[ "somefile" ] # TODO: The add gui will also add this
+files_folder="test" # TODO: The add gui with input folder settings will add this
+extensions=[ '.png', '.pdf' ] # TODO: The add gui will add this
+for file in listdir(files_folder):
+    for character in range(len(file)):
+        for keyword in range(len(keywords)):
+            if file[character:].startswith(keywords[keyword]):
+                file_type=path.splitext(file)[1].lower()
+                if file_type in extensions:
+                    print(file[character:])
+                    break
+    
+'''
 desktop = "D:\\Desktop"
 fileTypes = {  "Pdf's":".pdf", "Docx":".docx", "Exe":".exe", "Txt":".txt", "Wav":".wav",
                 "Msi":".msi", "Xpf":".xpf", "Zip":[".zip", ".tgz"], "McPack": ".mcpack"}
